12906437a42a58a8e006645593d737c9ebe95337
Joao Guilherme <joao.guilherme100@hotmail.com>
2023-03-31 11:02:16 -0300

    Consumindo uma API de filmes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        // fazer uma conexão http e buscar os top 10 filmes
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_syzbu08z";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // pegar só os dados que interessam(titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilme = parser.parse(body);
+
+        //exibir e manipular os dados
+
+        for (Map<String,String> filme : listaDeFilme) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
